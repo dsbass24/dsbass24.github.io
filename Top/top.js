bec210f4a4379c10dd750dee5bbd4c8e2f32596d
dsbass24 <47190756+dsbass24@users.noreply.github.com>
2022-05-29 12:28:59 +0100

    Tr

--- Top/top.js
+++ Top/top.js
@@ -8,11 +8,17 @@ document.addEventListener("DOMContentLoaded", function(e){
 	
 	var myBody = document.body;
 	
-	myBody.innerText = document.lastModified;
-	
+	//myBody.innerText = document.lastModified;
+	//===================================================================================================
 	function newTag(nameTag, idTag, classTag, contentTag){
 		var tag = document.createElement(nameTag);
 		myBody.appendChild(tag);
 		tag.id = idTag;
+		
 	}
+	
+	//===================================================================================================
+	
+	newTag("button", "upBut", "");
+	newTag("button", "downBut", "");
 });
