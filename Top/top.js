85e5668febecfed0acfa1afc0c030efc517f4cf9
dsbass24 <47190756+dsbass24@users.noreply.github.com>
2022-05-29 06:27:27 +0100

    First

---
+++ Top/top.js
@@ -0,0 +1,18 @@
+/*
+	Top page...
+	
+	JavaScript
+*/
+
+document.addEventListener("DOMContentLoaded", function(e){
+	
+	var myBody = document.body;
+	
+	myBody.innerText = document.lastModified;
+	
+	function newTag(nameTag, idTag, classTag, contentTag){
+		var tag = document.createElement(nameTag);
+		myBody.appendChild(tag);
+		tag.id = idTag;
+	}
+}
