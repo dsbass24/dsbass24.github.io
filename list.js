8c2fe6f5b01229c8b31933f39943d7c64a0d2ec0
dsbass24 <47190756+dsbass24@users.noreply.github.com>
2023-11-26 01:53:10 +0000

    new

---
+++ list.js
@@ -0,0 +1,32 @@
+/*
+	list
+	
+	Списки.
+	
+*/
+var myLinks = [
+	{
+		name : "Note",
+		pagesLincs : "Note/index.html"
+	},
+	{
+		name : "ctrl",
+		pagesLincs : "ctrl/ctrl.html"
+	},
+	{
+		name : "MyHTML",
+		pagesLincs : "https://dsbass24.github.io/MyHTML/"
+	},
+	{
+		name : "",
+		pagesLincs : ""
+	},
+	{
+		name : "",
+		pagesLincs : ""
+	},
+	{
+		name : "",
+		pagesLincs : ""
+	}
+];
